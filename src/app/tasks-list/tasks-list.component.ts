1595684b62a7a2d572009c02119f61376aaa3dd4
bartez90 <mojcin1@o2.pl>
2019-05-30 11:44:55 +0200

    2-data-binding

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 // @Component({
 //     selector: 'app-tasks-list',
@@ -8,16 +8,36 @@ import { Component, OnInit } from '@angular/core';
 //     styles: ['div { color: yellow; }']
 //   })
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.scss']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.scss']
 })
 
 export class TasksListComponent implements OnInit {
+    // @ViewChild('newTaskInput') newTaskInput: ElementRef;
+    tasks: string[] = [];
+    day = 'Today';
+    canAddTask = false;
+    maxTasks = 4;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    getDay(day: string): string {
+        return day;
+    }
+
+    addTask(newTaskInput: HTMLInputElement) {
+        // const task = this.newTaskInput.nativeElement.value;
+        const task = newTaskInput.value;
+        this.tasks.push(task);
+        console.log(this.tasks);
+    }
+
+    checkInput(e) {
+        this.canAddTask = e.target.value ? true : false;
+    }
 
 }
